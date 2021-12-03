80368bac99b174ab61a1c0684a7840ac9add7279
Daria Sukhonina <dariasukhonina@gmail.com>
2021-12-03 16:50:10 +0300

    Refactor part crates into day crates; Use tests instead of executables

---
+++ day-1/src/lib.rs
@@ -0,0 +1,34 @@
+const INPUT: &'static str = include_str!("../input.txt");
+
+pub fn part_1() -> usize {
+    count_increases(&parse_input())
+}
+
+pub fn part_2() -> usize {
+    let local_sums = parse_input().windows(3).map(|w| w.iter().sum()).collect();
+    count_increases(&local_sums)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn pt1() {
+        assert_eq!(super::part_1(), 1521)
+    }
+
+    #[test]
+    fn pt2() {
+        assert_eq!(super::part_2(), 1543)
+    }
+}
+
+fn parse_input() -> Vec<u32> {
+    INPUT
+        .lines()
+        .map(|line| line.trim().parse().unwrap())
+        .collect()
+}
+
+fn count_increases(v: &Vec<u32>) -> usize {
+    v.windows(2).filter(|w| w[0] < w[1]).count()
+}
---
+++ day-2/src/lib.rs
@@ -0,0 +1,76 @@
+const INPUT: &str = include_str!("../input.txt");
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn pt1() {
+        assert_eq!(super::part_1(), 1507611);
+    }
+
+    #[test]
+    fn pt2() {
+        assert_eq!(super::part_2(), 1880593125);
+    }
+}
+
+pub fn part_1() -> i32 {
+    let [ans_h, ans_d] = parse_input()
+        .into_iter()
+        .map(|(cmd, n)| match cmd {
+            Command::Forward => [n, 0],
+            Command::Down => [0, n],
+            Command::Up => [0, -n],
+        })
+        .reduce(|[h1, d1], [h2, d2]| [h1 + h2, d1 + d2])
+        .unwrap();
+
+    ans_h * ans_d
+}
+
+pub fn part_2() -> i32 {
+    let mut horiz = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for (cmd, n) in parse_input() {
+        match cmd {
+            Command::Forward => {
+                horiz += n;
+                depth += aim * n;
+            }
+            Command::Down => aim += n,
+            Command::Up => aim -= n,
+        }
+    }
+
+    horiz * depth
+}
+
+fn parse_input() -> Vec<(Command, i32)> {
+    INPUT
+        .lines()
+        .map(|line| {
+            let (cmd, n) = line.split_once(' ').unwrap();
+            (cmd.parse().unwrap(), n.parse().unwrap())
+        })
+        .collect()
+}
+
+enum Command {
+    Forward,
+    Down,
+    Up,
+}
+
+impl std::str::FromStr for Command {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "forward" => Ok(Command::Forward),
+            "down" => Ok(Command::Down),
+            "up" => Ok(Command::Up),
+            _ => Err(()),
+        }
+    }
+}
