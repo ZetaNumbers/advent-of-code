e6910763fa331693f5aed7673c18fa7a5626a349
Daria Sukhonina <dariasukhonina@gmail.com>
2021-12-07 00:04:30 +0300

    Solve day-3 part-2

--- day-3/src/lib.rs
+++ day-3/src/lib.rs
@@ -1,63 +1,146 @@
-use nalgebra::{SMatrix, SVector};
+use nalgebra as na;
 
-const INPUT: &str = include_str!("../input.txt");
+pub const INPUT: &str = include_str!("../input.txt");
+pub const EXAMPLE_INPUT: &str = include_str!("../example.txt");
 
-pub fn part_1() -> u32 {
-    let input = parse_input();
+pub fn parse_input(s: &str) -> na::DMatrix<bool> {
+    let nrows = match s.lines().next() {
+        Some(line) => line.len(),
+        None => return na::DMatrix::from_element(0, 0, false),
+    };
 
-    let hist: SMatrix<u32, 12, 2> = input
-        .map(|word| SMatrix::from_columns(&[word.map(|x| !x as u32), word.map(|x| x as u32)]))
-        .sum();
+    let mut data = Vec::with_capacity(nrows);
+    for line in s.lines() {
+        assert_eq!(line.len(), nrows);
+        data.extend(parse_word(line));
+    }
 
-    let gamma = word_to_u32(&SVector::from_iterator(
-        hist.row_iter().map(|h| h[0] < h[1]),
-    ));
-    let epsilon = word_to_u32(&SVector::from_iterator(
-        hist.row_iter().map(|h| h[0] > h[1]),
-    ));
+    assert_eq!(data.len() % nrows, 0);
+    let ncols = data.len() / nrows;
 
-    assert_eq!(!gamma & 0xfff, epsilon);
+    return na::DMatrix::from_vec(nrows, ncols, data);
+
+    fn parse_word<'a>(s: &'a str) -> impl Iterator<Item = bool> + 'a {
+        s.bytes().map(|ch| match ch {
+            b'0' => false,
+            b'1' => true,
+            _ => unreachable!(),
+        })
+    }
+}
+
+pub fn part_1(input: na::DMatrix<bool>) -> u32 {
+    let mut histogram = na::MatrixXx2::from_element(input.nrows(), 0_u32);
+    let mut delta = na::MatrixXx2::from_element(input.nrows(), 0);
+    for word in input.column_iter() {
+        let mut word_iter = word.iter();
+        delta
+            .column_mut(0)
+            .as_mut_slice()
+            .fill_with(|| !*word_iter.next().unwrap() as u32);
+        let mut word_iter = word.iter();
+        delta
+            .column_mut(1)
+            .as_mut_slice()
+            .fill_with(|| *word_iter.next().unwrap() as u32);
+        histogram += &delta;
+    }
+
+    let gamma = word_to_number(
+        histogram
+            .row_iter()
+            .map(|stats| most_common_bit(stats[0], stats[1], |_| panic!())),
+    );
+    let epsilon = word_to_number(
+        histogram
+            .row_iter()
+            .map(|stats| least_common_bit(stats[0], stats[1], |_| panic!())),
+    );
 
     gamma * epsilon
 }
 
-fn parse_input() -> impl Iterator<Item = SVector<bool, 12>> {
-    INPUT.lines().map(parse_word)
-}
-
-fn parse_word(s: &str) -> SVector<bool, 12> {
-    let mut iter = s.bytes().map(|ch| match ch {
-        b'0' => false,
-        b'1' => true,
-        _ => unreachable!(),
+pub fn part_2(input: na::DMatrix<bool>) -> u32 {
+    let oxygen_rating = gas_rating(input.clone(), |zeros, ones| {
+        most_common_bit(zeros, ones, |_| true)
     });
-    let out = SVector::from_iterator(&mut iter);
-    assert_eq!(iter.next(), None, "word should only be 12 bits long");
-    out
+    let co2_rating = gas_rating(input, |zeros, ones| {
+        least_common_bit(zeros, ones, |_| false)
+    });
+
+    let oxygen_rating = word_to_number(oxygen_rating.iter().copied());
+    let co2_rating = word_to_number(co2_rating.iter().copied());
+    return oxygen_rating * co2_rating;
+
+    fn gas_rating<F>(mut input: na::DMatrix<bool>, mut criteria: F) -> na::DVector<bool>
+    where
+        F: FnMut(u32, u32) -> bool,
+    {
+        let mut filter = na::DVector::from_element(0, false);
+
+        loop {
+            match input.ncols() {
+                0 => unreachable!(),
+                1 => break input.column(0).into(),
+                _ => (),
+            }
+
+            let stats = input
+                .row(filter.nrows())
+                .fold(na::vector![0, 0], |acc, bit| {
+                    acc + if bit {
+                        na::vector![0, 1]
+                    } else {
+                        na::vector![1, 0]
+                    }
+                });
+
+            filter = filter.push(criteria(stats[0], stats[1]));
+
+            let mut data = Vec::with_capacity(input.nrows() * input.ncols());
+            for w in input.column_iter() {
+                if !w.as_slice().starts_with(filter.as_slice()) {
+                    continue;
+                }
+                data.extend_from_slice(w.as_slice());
+            }
+
+            assert_eq!(data.len() % input.nrows(), 0);
+            let ncols = data.len() / input.nrows();
+            input = na::DMatrix::from_vec(input.nrows(), ncols, data);
+        }
+    }
 }
 
-fn word_to_u32(v: &SVector<bool, 12>) -> u32 {
-    v.iter().fold(0, |acc, &bit| acc << 1 | bit as u32)
+fn word_to_number(word: impl Iterator<Item = bool>) -> u32 {
+    word.fold(0, |acc, bit| acc << 1 | bit as u32)
+}
+
+fn most_common_bit<F>(zeros: u32, ones: u32, on_equals: F) -> bool
+where
+    F: FnOnce(u32) -> bool,
+{
+    match [zeros, ones] {
+        [a, b] if a == b => on_equals(a),
+        stats => std::cmp::max_by_key(false, true, |&i| stats[i as usize]),
+    }
+}
+
+fn least_common_bit<F>(zeros: u32, ones: u32, on_equals: F) -> bool
+where
+    F: FnOnce(u32) -> bool,
+{
+    match [zeros, ones] {
+        [a, b] if a == b => on_equals(a),
+        [0, _] | [_, 0] => unimplemented!(),
+        stats => std::cmp::min_by_key(false, true, |&i| stats[i as usize]),
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn input_is_ascii() {
-        assert!(INPUT.is_ascii())
-    }
-
-    #[test]
-    fn parsing_word() {
-        let s = "010011010000";
-        let out = format!("{:012b}", word_to_u32(&parse_word(s)));
-        assert_eq!(s, out);
-    }
-
-    #[test]
-    fn pt1() {
-        assert_eq!(part_1(), 2724524)
+        assert!(super::INPUT.is_ascii())
     }
 }
---
+++ day-3/tests/answer.rs
@@ -0,0 +1,11 @@
+use day_3::{parse_input, INPUT};
+
+#[test]
+fn part_1() {
+    assert_eq!(day_3::part_1(parse_input(INPUT)), 2724524)
+}
+
+#[test]
+fn part_2() {
+    assert_eq!(day_3::part_2(parse_input(INPUT)), 0)
+}
